17e9e60e87261a4d12e6b2629184cf2919a055e1
Gatewaynode <44749714+gatewaynode@users.noreply.github.com>
2021-06-25 17:20:53 -0400

    Added binary file test and hexyl for binary viewing.

--- opener.bash
+++ opener.bash
@@ -3,5 +3,10 @@
 if [ -d "$1" ]; then
 	tree -L 3 "$1"
 else
-	bat --color=always --style=numbers --line-range=:500 "$1"
+	BAT_TEST=$(file --mime "$1")
+	if [[ $BAT_TEST == *binary ]] ; then
+		hexyl "$1"
+	else
+		bat --color=always --style=numbers --line-range=:500 "$1"
+	fi
 fi
